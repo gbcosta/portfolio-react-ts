a58f80be313bbc6c35a3a6da70c7ee97f730867c
Gabriel <gbcostasant@gmail.com>
2022-10-15 18:46:42 -0300

    Projects responsivity

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Progress, Text, useColorMode } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 import { MarginPage } from "@components/MarginComponent";
 import { ProjectCard } from "@components/projectCard/projectCard";
 
@@ -7,7 +7,13 @@ export const Projects = (): JSX.Element => {
     <MarginPage>
       <Heading size={"3xl"}>Projects</Heading>
       <Text fontSize={"xl"} sx={{ mt: 8 }}></Text>
-      <Flex gap={1} sx={{ flexDirection: "row", justifyContent: "center" }}>
+      <Flex
+        gap={1}
+        sx={{
+          flexDirection: { base: "column", xl: "row" },
+          justifyContent: "center",
+        }}
+      >
         <Flex gap={1} sx={{ flexDir: "column" }}>
           <ProjectCard size={"extraSmall"}></ProjectCard>
           <ProjectCard></ProjectCard>
